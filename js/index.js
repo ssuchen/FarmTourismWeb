1c287cffea29adcfb60f9455071cdf1db3083cc9
ssuchen <48403268+ssuchen@users.noreply.github.com>
2019-08-29 16:27:02 +0800

    將最新消息放入 index 頁

--- js/index.js
+++ js/index.js
@@ -6,24 +6,33 @@
 });
 
 function newRender(data){
- console.log(data)
  //let len = data.XML_Head.Infos.Info.length
-for( let i=0; i<8 ;i++ ){
-
+for( let i=0; i<6 ;i++ ){
    //最新消息 資料  
-    let title = data.XML_Head.Infos.Info[i].Name
-    let text = data.XML_Head.Infos.Info[i].Description.slice(0,30)+"..."
-    let time = data.XML_Head.Infos.Info[i].Start.slice(0,10)
-    console.log(text)
+    let title = data.XML_Head.Infos.Info[i].Name;
+    let text = data.XML_Head.Infos.Info[i].Description.slice(0,50)+"...";
+    let time = data.XML_Head.Infos.Info[i].Start.slice(0,10);
     
     //最新消息DOM元素
      let indexMainCrad = document.createElement('div');
          indexMainCrad.setAttribute('class','index-main-crad');
      let cardTitle = document.createElement('div');
          cardTitle.setAttribute('class','card-title');
-     let cardtext = document.createElement('p');
-     
-     
+         cardTitle.textContent = title;
+     let cardText = document.createElement('p');
+         cardText.textContent = text ;
+     let cardTime = document.createElement('p');
+         cardTime.setAttribute('class','card-time');
+         cardTime.textContent = time ;
+
+
+
+     let indexMainRight = document.querySelector('.index-main-right');
+     indexMainRight.appendChild(indexMainCrad);
+     indexMainCrad.appendChild(cardTitle);
+     indexMainCrad.appendChild(cardText);
+     indexMainCrad.appendChild(cardTime);
+
 
 
 }
