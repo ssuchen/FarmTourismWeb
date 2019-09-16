f54f68513b3b9e91b080e23dc5e67090ebd94acd
ssuchen <48403268+ssuchen@users.noreply.github.com>
2019-09-16 10:39:50 +0800

    增加 navbar

--- public/js/login.js
+++ public/js/login.js
@@ -8,31 +8,32 @@ var firebaseConfig = {
     appId: "1:162491477012:web:c50ba3d9fca5ed5e3e0b01"
 };
 
-    firebase.initializeApp(firebaseConfig );  
+    firebase.initializeApp(firebaseConfig);  
 
     var uiConfig = {  
-    signInSuccessUrl: 'loginpage.html',  
+    signInSuccessUrl: 'index.html', 
     signInOptions: [  
      // Specify providers you want to offer your users.  
-     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-     firebase.auth.FacebookAuthProvider.PROVIDER_ID
+     firebase.auth.GoogleAuthProvider.PROVIDER_ID
+     //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
      //firebase.auth.EmailAuthProvider.PROVIDER_ID  
     ],  
     // Terms of service url can be specified and will show up in the widget.  
     tosUrl: '<your-tos-url>'  
     };  
+    
     // Initialize the FirebaseUI Widget using Firebase.  
     var ui = new firebaseui.auth.AuthUI(firebase.auth());  
     // The start method will wait until the DOM is loaded.  
     ui.start('#firebaseui-auth-container', uiConfig);  
 
-    
+    console.log(uiConfig)
     //顯示與關閉 登入對話框
     let loginBox
     window.onload = function(){
        loginBox = document.querySelector(".login-box"); 
     } 
-    let loginBtn = document.querySelector('.loginbtn');    
+    let loginBtn = document.querySelector('.loginbtn-user');    
         loginBtn.addEventListener('click',function(){ 
         loginBox.style.display="block";
     })
@@ -41,3 +42,6 @@ var firebaseConfig = {
         loginBox.style.display="none";
         
     })
+
+
+    
